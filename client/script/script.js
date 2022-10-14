5afeac051a285f18d31bcd0201d2af2f885cac1c
Jonta Sancar <109826535+Jonta-Sancar@users.noreply.github.com>
2022-10-14 11:09:52 -0300

    adjust header and css

--- client/script/script.js
+++ client/script/script.js
@@ -1,132 +1,37 @@
-var hideInterval;
-let board_active = {
-    e: null,
-    d: null
-};
-let board_height = 0;
-let change_height_time = 3;
+const header = document.querySelector('header');
+const headerClass = header.classList;
+let prev = 0;
+addEventListener('scroll', () => {
 
-const active = {
-    remove(e, d, callback) {
-
-        e.classList.remove('active');
-
-        displayControl.hide(d, callback);
-
-    },
-    add(e, d) {
-
-        e.classList.add('active');
-
-        d.style.display = 'flex';
-
-        displayControl.show(d);
-
-        board_height = d.dataset.size;
+  if (scrollY >= prev) {
+    if (!headerClass.contains('headerHide')) {
+      headerClass.add('headerHide');
+      header.style.top = '-100px';
     }
-}
-const displayControl = {
-    show(d) {
-        clearInterval(hideInterval);
-
-        let height = 0;
-        let padding = 0;
-
-        hideInterval = setInterval(() => {
-
-            if (height <= board_height) {
-
-                d.style.height = height + 'px';
-                height += change_height_time;
-            }
-            else {
-
-                clearInterval(hideInterval);
-            }
-            if (padding <= 10) {
-
-                d.style.padding = padding + 'px';
-                padding += change_height_time;
-            }
-        }, 0.1)
-    },
-    hide(d, callback) {
-        clearInterval(hideInterval);
-
-        let height = board_height;
-        let padding = 10;
-
-        hideInterval = setInterval(() => {
-
-            if (height >= 0) {
-
-                d.style.height = height + 'px';
-                height -= change_height_time;
-            }
-            else {
-
-                d.style.display = 'none';
-                clearInterval(hideInterval);
-
-                if (callback) {
-                    callback();
-                }
-            }
-            if (padding >= 0) {
-
-                d.style.padding = padding + 'px';
-                padding -= change_height_time;
-            }
-        }, 0.1)
-    }
-}
+  }
+  else if (headerClass.contains('headerHide')) {
+    headerClass.remove('headerHide');
+    header.style.top = `0px`;
+  }
+  prev = scrollY;
+})
 
 function toActive(e) {
 
-    let displayId = e.getAttribute('href');
-    if (displayId.split('://').length == 1) {
+    const active_element = document.querySelector('#nav-ul .active');
 
-        let display = document.querySelector(displayId);
-
-        if (display == board_active.d) {
-
-            active.remove(e, display)
-            board_active.e = null;
-            board_active.d = null;
-        }
-        else {
-
-            if (board_active.e != e && board_active.e != null) {
-
-                active.remove(board_active.e, board_active.d, () => {
-
-                    active.add(e, display)
-                    board_active.e = e;
-                    board_active.d = display;
-                });
-            }
-            else {
-
-                active.add(e, display)
-                board_active.e = e;
-                board_active.d = display;
-            }
-
-        }
+    if (e.href != active_element.href) {
+        
+        active_element.classList.remove('active');
+        e.classList.add('active');
     }
-}
 
-onload = function () {
-    const dropdown = document.querySelectorAll('.dropdown');
+    const burger = document.querySelector('#burger-active');
 
-    dropdown.forEach(element => {
-        element.dataset.size = element.clientHeight;
+    if(burger){
 
-        element.style.height = '0px';
-        element.style.padding = '0px';
-        element.style.display = 'none';
-    });
-
+        toggleMenu(burger);
+    }
 }
 
 function toggleMenu(e) {
